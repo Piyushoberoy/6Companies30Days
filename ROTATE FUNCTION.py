c5fb4352c6af0ff8dc74ca45f29f37d6a8506b34
Piyush <68097992+Piyushoberoy@users.noreply.github.com>
2023-01-03 11:10:36 +0530

    Create ROTATE FUNCTION.py

---
+++ ROTATE FUNCTION.py
@@ -0,0 +1,19 @@
+# PROBLEM STATEMENT:-
+# You are given an integer array nums of length n. Return maximum length of Rotation Function.
+
+# LINK:-
+# https://leetcode.com/problems/rotate-function/
+
+# CODE:-
+class Solution:
+    def maxRotateFunction(self, nums: List[int]) -> int:
+        sums = c_sum = sum(idx * ele for idx, ele in enumerate(nums))
+        s = sum(nums)
+        n = len(nums)
+        while nums:
+            c_sum += s - nums.pop() * n
+            sums = max(c_sum, sums)
+        return sums
+
+# TIME COMPLEXITY
+# O(n)
